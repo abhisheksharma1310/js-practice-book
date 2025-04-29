b9a488e2138cba0821bf7791a9085095b049acaf
abhisheksharma <abhisheksharma131098@gmail.com>
2025-04-29 19:56:28 +0530

    create simple html page to show api data in a table using dom

---
+++ frontend/show-data/index.js
@@ -0,0 +1,40 @@
+const url = "https://jsonplaceholder.typicode.com/users";
+let data = [];
+
+const fetchData = async () => {
+  try {
+    const reponse = await fetch(url);
+    data = await reponse.json();
+    data.length > 0 ? displayData() : console.log("No data found");
+    console.log("Data fetched successfully:", data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+fetchData();
+
+function displayData() {
+  const table = document.createElement("table");
+  table.innerHTML = `
+        <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+        </tr>
+        ${data
+          ?.map((user) => {
+            return `
+                <tr>
+                    <td>${user.name}</td>
+                    <td>${user.username}</td>
+                    <td>${user.email}</td>
+                    <td>${user.phone}</td>
+                    <td>${user.website}</td>
+                </tr>`;
+          })
+          .join("")}
+        </tr>`;
+  document.body.appendChild(table);
+}
