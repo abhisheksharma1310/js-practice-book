f7bd285ec500ec55cb407511863d4e8e94ffeb98
abhisheksharma <abhisheksharma131098@gmail.com>
2025-07-05 17:53:36 +0530

    api created

---
+++ FreeCodeCamp/Back End Development and APIs/timestamp/index.js
@@ -0,0 +1,54 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+//api/
+app.get("/api/", function (req, res) {
+  const currentDate = new Date();
+  const dateString = {
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString(),
+  };
+  res.json(dateString);
+});
+
+///api/:date
+app.get("/api/:date", function (req, res) {
+  const { date } = req.params;
+  const isDate = isNaN(date) ? new Date(date) : new Date(Number(date));
+  if (isDate) {
+    const dateString = {
+      unix: isDate.getTime(),
+      utc: isDate.toUTCString(),
+    };
+    res.json(dateString);
+  } else {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
